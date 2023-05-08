b9f9e11e8039426f99b75bc6551048c7c9fa4464
JamelSaab <128429161+JamelSaab@users.noreply.github.com>
2023-05-08 15:05:45 -0700

    5/8

--- GroupPlatformer/Assets/Jamel/Scripts/Boss_Chase.cs
+++ GroupPlatformer/Assets/Jamel/Scripts/Boss_Chase.cs
@@ -50,14 +50,5 @@ public class Boss_Chase : MonoBehaviour
     {
 
     }
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
